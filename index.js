e54a503a8408ff71ac28ccc8fbb6a94a3839c374
Wael Almattar <waelsy123@gmail.com>
2020-07-01 20:15:31 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = process.env.port || 3000
+
+let ip = ''
+
+app.get('/ping', (req, res) => {
+  ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
+  return res.send('Hello World!')
+})
+
+app.get('/', (req, res) => res.send(`his ip: ${ip}`))
+
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`)
+)
